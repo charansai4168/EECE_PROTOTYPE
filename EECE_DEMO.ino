97c27e7a3a63d4b628c308d002b5389fcc448b4f
Charan Sai. Gajulapuram <134192506+charansai4168@users.noreply.github.com>
2025-01-06 19:53:00 +0530

    Add files via upload
    Arduino programming for EECE prototype.

---
+++ EECE_DEMO.ino
@@ -0,0 +1,44 @@
+////EECE_DEMO
+
+const int led = 6; /// declaring led at digital pmw6 
+const int temp = A0; ///declaring the temp sensor pin at A0 to access signal
+ 
+/*
+setting up a threshold temp which less than melting point for demo
+*/
+float threshold = 60.0;
+
+void setup(){
+
+  pinMode(led, OUTPUT);
+  Serial.begin(9600);
+  Serial.print("Done by:B22020\n");
+}
+
+void loop(){
+
+  int data = analogRead(temp); ///Reading temp from sensor 
+  /*
+  Convertion raw data into temperature 
+  */
+  float voltage = data * (5.0 / 1023.0);
+  float readtemp = (voltage - 0.5) * 100.0;
+  
+  /*
+  comapring the temp with threshold temp to make circuit break
+   */
+
+  if(threshold < readtemp){ 
+  
+    digitalWrite(led, LOW);
+    Serial.println("Temperature is high");
+    
+  }
+  else{
+  
+    digitalWrite(led, HIGH);
+    Serial.println("Optimal Temperature:");
+    Serial.print(readtemp);
+    
+  }
+}
